b668da5bc6b9eb540e48e605f0bf44b7ee2a39da
Jonathan Tan <jonathantjm@u.nus.edu>
2020-07-25 19:56:35 +0800

    remove redundant code

--- i2emr/imports/ui/Form.jsx
+++ i2emr/imports/ui/Form.jsx
@@ -200,27 +200,12 @@ class Form extends Component {
         </div>
         
         <br /><Divider />
-        {/* {typeof(this.props.stationQueue) !== "undefined" &&
-          <Typography variant="h6">
-            Next station: {this.props.stationQueue[1]}
-          </Typography>
-        } */}
       </ClearableAutoForm>
     );
     
-    // Replace undefined with default value of 0
-    const tabValue = (this.state.tabValue === undefined) ? 0 : this.state.tabValue;
-    
     return (
       <Paper elevation={2} p={0} m={0}>
-        <AppBar position="static" color="default">
-          {/* <Tabs value={tabValue} onChange={this.handleTabChange}>
-            <Tab label="Form" />
-            <Tab label="Stations" />
-          </Tabs> */}
-        </AppBar>
-        {tabValue === 0 && newForm()}
-        {tabValue === 1 && typeof(this.props.stationQueue) !== "undefined" && this.getStationList()}
+        {newForm()}
       </Paper>
     );
   }
